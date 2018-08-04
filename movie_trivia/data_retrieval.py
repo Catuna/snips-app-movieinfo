a5c2a283c78d142ea9b9cd4507e47365e87a2740
Christian Barth Roligheten <chrbarrol@gmail.com>
2018-08-04 18:11:28 +0200

    Implemented movie search and retrieval function

--- movie_trivia/data_retrieval.py
+++ movie_trivia/data_retrieval.py
@@ -1,5 +1,18 @@
+import tmdbsimple as tmdb
+
+tmdb.API_KEY = '3f5e463e7b648ef4b2d6f884a81b044f'
+
+
 def retrieveMostRelevantMediaFromQuery(query):
-    raise Exception('Not implemented')
+    search = tmdb.Search()
+    searchResponse = search.movie(query=query)
+
+    if searchResponse['total_results'] == 0:
+        return None
+    bestMatchId = searchResponse['results'][0]['id']
+
+    movieResponse = tmdb.Movies(bestMatchId).info()
+    return movieResponse
 
 
 def retrieveRandomGoodMovieFromGenres(genres):
